4af5bea9045b7fc0fc4fb024862cb7adff257308
王泽宇 <2231777940@qq.com>
2023-01-30 18:40:01 +0800

    ArrayStack

---
+++ Stack/ArrayStack.java
@@ -0,0 +1,73 @@
+package Stack;
+
+public class ArrayStack<AnyType> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    //------------------------------------------------------------------------------------
+    //--------------------------------| member variables |--------------------------------
+    //------------------------------------------------------------------------------------
+
+
+    private AnyType[] stack;
+    private int topOfStack;
+
+
+    //--------------------------------------------------------------------------------
+    //--------------------------------| constructors |--------------------------------
+    //--------------------------------------------------------------------------------
+
+
+    public ArrayStack() { this( DEFAULT_CAPACITY ); }
+    public ArrayStack( int capacity ) { stack = (AnyType[]) new Object[ capacity ]; topOfStack = -1; }
+
+
+    //----------------------------------------------------------------------------------
+    //--------------------------------| public methods |--------------------------------
+    //----------------------------------------------------------------------------------
+
+
+    public int size() { return topOfStack + 1; }
+
+    public boolean isEmpty() { return topOfStack == -1; }
+
+    public void clear() { stack = (AnyType[]) new Object[ DEFAULT_CAPACITY ]; topOfStack = -1; }
+
+    //----------------------------------------------------------------------------------
+
+    public AnyType top() {
+        if( isEmpty() )
+            throw new java.util.EmptyStackException();
+
+        return stack[ topOfStack ];
+    }
+
+    public void push( AnyType data ) {
+        if( size() == stack.length )
+            ensureCapacity( size() * 2 + 1 );
+
+        stack[ ++topOfStack ] = data;
+    }
+
+    public AnyType pop() {
+        if( isEmpty() )
+            throw new java.util.EmptyStackException();
+
+        return stack[ topOfStack-- ];
+    }
+
+    //----------------------------------------------------------------------------------
+
+    public void ensureCapacity( int capacity ) {
+        if( capacity < size() )
+            return ;
+
+        AnyType[] old = stack;
+        stack = (AnyType[]) new Object[ capacity ];
+        for( int i=0; i < size(); i++)
+            stack[i] = old[i];
+    }
+
+    public void trimToSize() { ensureCapacity( size() ); }
+
+}
